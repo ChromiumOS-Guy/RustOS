1cda02698b8eb841d687c5cf6bec086bc865962f
ChromiumOS-Guy <alon.arvatz109@gmail.com>
2023-08-12 21:11:22 +0300

    Fixed Rstr

--- kernel/src/Rstr.rs
+++ kernel/src/Rstr.rs
@@ -9,94 +9,129 @@
     unused_mut
 )]
 
-
 pub fn u64_to_str(mut value: u64) -> String {
     let mut int_to_string_output = String::with_capacity(128);
     while value / 10 > 0 {
-        let reminder: u8 = lazy_conversion(value % 10);
+        let reminder: char = lazy_conversion(value % 10);
         value /= 10;
-        int_to_string_output.push((reminder + '0' as u8) as char);
+        int_to_string_output.push(reminder);
     }
 
-    let reminder: u8 = lazy_conversion(value % 10);
-    int_to_string_output.push((reminder + '0' as u8) as char);
-    return int_to_string_output;
+    let reminder: char = lazy_conversion(value % 10);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
 }
 
 pub fn usize_to_str(mut value: usize) -> String {
     let mut int_to_string_output = String::with_capacity(128);
     while value / 10 > 0 {
-        let reminder: u8 = lazy_conversion((value % 10) as u64);
+        let reminder: char = lazy_conversion((value % 10) as u64);
         value /= 10;
-        int_to_string_output.push((reminder + '0' as u8) as char);
+        int_to_string_output.push(reminder);
     }
 
-    let reminder: u8 = lazy_conversion((value % 10) as u64);
-    int_to_string_output.push((reminder + '0' as u8) as char);
-    return int_to_string_output;
+    let reminder: char = lazy_conversion((value % 10) as u64);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
 }
 
 pub fn u32_to_str(mut value: u32) -> String {
     let mut int_to_string_output = String::with_capacity(128);
     while value / 10 > 0 {
-        let reminder: u8 = lazy_conversion((value % 10) as u64);
+        let reminder: char = lazy_conversion((value % 10) as u64);
         value /= 10;
-        int_to_string_output.push((reminder + '0' as u8) as char);
+        int_to_string_output.push(reminder);
     }
 
-    let reminder: u8 = lazy_conversion((value % 10) as u64);
-    int_to_string_output.push((reminder + '0' as u8) as char);
-    return int_to_string_output;
+    let reminder: char = lazy_conversion((value % 10) as u64);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
 }
 
 pub fn u16_to_str(mut value: u16) -> String {
     let mut int_to_string_output = String::with_capacity(128);
     while value / 10 > 0 {
-        let reminder: u8 = lazy_conversion((value % 10) as u64);
+        let reminder: char = lazy_conversion((value % 10) as u64);
         value /= 10;
-        int_to_string_output.push((reminder + '0' as u8) as char);
+        int_to_string_output.push(reminder);
     }
 
-    let reminder: u8 = lazy_conversion((value % 10) as u64);
-    int_to_string_output.push((reminder + '0' as u8) as char);
-    return int_to_string_output;
+    let reminder: char = lazy_conversion((value % 10) as u64);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
 }
 
 pub fn u8_to_str(mut value: u8) -> String { 
     let mut int_to_string_output = String::with_capacity(128); 
     while value / 10 > 0 { 
-        let reminder = value % 10;
+        let reminder : char = lazy_conversion((value % 10) as u64);
         value /= 10;
-        int_to_string_output.push((reminder + '0' as u8) as char);
+        int_to_string_output.push(reminder);
     }
 
-    let reminder = value % 10; 
-    int_to_string_output.push((reminder + '0' as u8) as char);
-    return int_to_string_output;
+    let reminder : char = lazy_conversion((value % 10) as u64);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
 }
 
-fn lazy_conversion(value: u64) -> u8 {
-    // temporary fix until i care enough (which is never)
-    if value == 0 {
-        return 0u8;
-    } else if value == 1 {
-        return 1u8;
-    } else if value == 2 {
-        return 2u8;
-    } else if value == 3 {
-        return 3u8;
-    } else if value == 4 {
-        return 4u8;
-    } else if value == 5 {
-        return 5u8;
-    } else if value == 6 {
-        return 6u8;
-    } else if value == 7 {
-        return 7u8;
-    } else if value == 8 {
-        return 8u8;
-    } else if value == 9 {
-        return 9u8;
+pub fn u64hex_to_str(mut value: u64) -> String {
+    let mut int_to_string_output = String::with_capacity(128);
+    while value / 16 > 0 {
+        let reminder: char = lazy_conversion(value % 16);
+        value /= 16;
+        int_to_string_output.push(reminder);
     }
-    return 0u8; //defualts to a 0
+
+    let reminder: char = lazy_conversion(value % 16);
+    int_to_string_output.push(reminder);
+    return ReverseString(int_to_string_output);
+}
+
+fn lazy_conversion(value: u64) -> char {
+    if value == 0 {
+        return '0';
+    } else if value == 1 {
+        return '1';
+    } else if value == 2 {
+        return '2';
+    } else if value == 3 {
+        return '3';
+    } else if value == 4 {
+        return '4';
+    } else if value == 5 {
+        return '5';
+    } else if value == 6 {
+        return '6';
+    } else if value == 7 {
+        return '7';
+    } else if value == 8 {
+        return '8';
+    } else if value == 9 {
+        return '9';
+    } else if value == 10 {
+        return 'A';
+    } else if value == 11 {
+        return 'B';
+    } else if value == 12 {
+        return 'C';
+    } else if value == 13 {
+        return 'D';
+    } else if value == 14 {
+        return 'E';
+    } else if value == 15 {
+        return 'F';
+    }
+    return '0'; //defualts to a 0
+}
+
+fn ReverseString(string: String) -> String { // weird stuff but this apparently has no memory leaks 
+    let mut int_to_string = Vec::<char>::with_capacity(128);
+    for c in string.chars().rev() {
+        int_to_string.push(c)
+    }
+    let mut int_to_string_output2 = String::with_capacity(128);
+    for c in int_to_string {
+        int_to_string_output2.push(c);
+    }
+    return int_to_string_output2;
 }
--- kernel/src/kernel.rs
+++ kernel/src/kernel.rs
@@ -60,31 +60,38 @@ extern "C" fn _start(bootInfo:*mut BootInfo) {
     Print(r"2. IDT I need it so I can do shit with PIT when I make IO lib");
     Next();
     Next();
-    Print("Testing Rstr should be 100:   ");
-    Print(&Rstr::u8_to_str(100u8));
-    Next();
     Print(r"RAM_INFO:");
     Next();
-    Print(r"freeMemory: ");
+    Print(r"FreeRAM: ");
     Print(&Rstr::usize_to_str(PageFrameAllocator::GetFreeRAM()));
     Next();
-    Print(r"usedMemory: ");
+    Print(r"UsedRAM: ");
     Print(&Rstr::usize_to_str(PageFrameAllocator::GetUsedRAM()));
     Next();
-    Print(r"reservedMemory: ");
+    Print(r"ReservedRAM: ");
     Print(&Rstr::usize_to_str(PageFrameAllocator::GetReservedRAM()));
     Next();
     Print("Requesting 20 Pages in Memory: ");
     for i in 0..20{
         Next();
         let address : *mut std::ffi::c_void = unsafe {PageFrameAllocator::RequestPage()};
-        Print(&Rstr::u64_to_str(address as u64));
+        Print("0x");
+        Print(&Rstr::u64hex_to_str(address as u64));
 
 
     }
     Next();
-    Print("usedMemory: ");
+    Print(r"RAM_INFO After Page Requests:");
+    Next();
+    Print(r"FreeRAM: ");
+    Print(&Rstr::usize_to_str(PageFrameAllocator::GetFreeRAM()));
+    Next();
+    Print(r"UsedRAM: ");
     Print(&Rstr::usize_to_str(PageFrameAllocator::GetUsedRAM()));
+    Next();
+    Print(r"ReservedRAM: ");
+    Print(&Rstr::usize_to_str(PageFrameAllocator::GetReservedRAM()));
+    Next();
     loop {
 
     }
